783d0ec8f9508a9ef3effbaf925a679d1d9394fb
z920804tw <129352148+z920804tw@users.noreply.github.com>
2025-04-07 11:55:37 +0800

    2025/4/7
    1.2048檔案上傳

---
+++ Assets/Scripts/TileSO/TileState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "TileState", menuName = "")]
+public class TileState : ScriptableObject
+{
+    public Color backgroundColor;
+    public Color textColor;
+}
